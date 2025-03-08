e6d4709eed6e17f4143f4fce8147cc0075f3d26a
Dhananjai <dhananjaiyadav2006@gmail.com>
2025-03-08 21:38:32 +0530

    done with the basic react component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomePage from './homePage'
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/homePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function homePage() {
+  return (
+    <>
+    <div>
+        <h1>Dhananjai Yadav</h1>
+        <h2>Contact: 9916294651</h2>
+    </div>
+    
+    
+    
+    
+    </>
+    
+  )
+}
+
+export default homePage
